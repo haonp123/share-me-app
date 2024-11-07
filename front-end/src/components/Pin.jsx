fc0b39ddb978adeb1decfb680ca0252a9ea356a8
NoPro <haonp123@gmail.com>
2024-11-07 22:24:13 +0700

    fix bug for deploying

--- front-end/src/components/Pin.jsx
+++ front-end/src/components/Pin.jsx
@@ -13,7 +13,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   const navigate = useNavigate();
 
   const user = fetchUser();
-  const alreadySaved = save?.filter((item) => item.postedBy._id === user.sub);
+  const alreadySaved = save?.filter((item) => item.postedBy._id === user?.sub);
 
   const savePin = (id) => {
     if (!alreadySaved) {
@@ -23,10 +23,10 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         .insert("after", "save[-1]", [
           {
             _key: uuidv4(),
-            userId: user.sub,
+            userId: user?.sub,
             postedBy: {
               _type: "postedBy",
-              _ref: user.sub,
+              _ref: user?.sub,
             },
           },
         ])
@@ -101,7 +101,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                   {destination?.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
                 </a>
               )}
-              {postedBy?._id === user.sub && (
+              {postedBy?._id === user?.sub && (
                 <button
                   type="button"
                   onClick={(e) => {
